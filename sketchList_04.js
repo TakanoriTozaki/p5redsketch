9352074a8b5e8a8140b2616b51c28bab843c6925
TakanoriTozaki <chappy622@gmail.com>
2018-05-26 13:14:06 +0900

    add file

--- sketchList_04.js
+++ sketchList_04.js
@@ -1,18 +1,20 @@
 var y;
 
 function setup() {
- createCanvas(720,400);
- stroke(255);
- noLoop();
+  createCanvas(720, 400);
+  // 線の色は白
+  stroke(255);
+  // ループはさせない
+  noLoop();
 
- y = height * 0.5;
+  y = height * 0.5;
 }
 
 function draw() {
- background(0);
- y = y - 1;
- if (y < 0){
-	 y = height;
- }
- line(0, y, width, y);
+  // 背景の色は黒
+  background(0);
+  y = y - 1;
+  if (y < 0) { y = height; }
+  // 線を描くyは動くwidthは720
+  line(0, y, width, y);
 }
