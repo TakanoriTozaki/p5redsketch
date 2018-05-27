71982eee9a9ca7a0772a26a3505abaeb234cb4c3
TakanoriTozaki <chappy622@gmail.com>
2018-05-27 11:53:18 +0900

    file add

--- sketchList_05.js
+++ sketchList_05.js
@@ -1,16 +1,19 @@
-var y = 360;
+var y = 100;
 
 function setup() {
- createCanvas(720,400);
- stroke(255);
- frameRate(30);
+  createCanvas(720, 400);
+  // 線の色
+  stroke(255);
+  // １秒間に30回
+  frameRate(30);
 }
 
 function draw() {
- background(0);
- y = y - 1;
- if (y < 0){
-	 y = height;
- }
- line(0, y, width, y);
+  //背景の色を黒
+  background(0);
+  y = y - 1;
+  if (y < 0) {
+    y = height;
+  }
+  line(0, y, width, y);
 }
