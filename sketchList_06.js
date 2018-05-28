f1b4fd5133005381d0cf631e414c123e46af6d75
TakanoriTozaki <chappy622@gmail.com>
2018-05-29 02:33:28 +0900

    file add

--- sketchList_06.js
+++ sketchList_06.js
@@ -1,21 +1,21 @@
 var y;
 
 function setup() {
- createCanvas(720,400);
- stroke(255);
- noLoop();
- y = height * 0.5;
+  createCanvas(720, 400);
+  stroke(255);
+  noLoop();
+  y = height * 0.5;
 }
 
 function draw() {
- background(0);
- y = y - 4;
- if (y < 0){
-	 y = height;
- }
- line(0, y, width, y);
+  background(0);
+  y = y - 4;
+  if (y < 0) {
+    y = height;
+  }
+  line(0, y, width, y);
 }
 
-function mouseClicked() {
-	redraw();
+function mousePressed() {
+  redraw();
 }
