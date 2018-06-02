fcd41d14bb8418347ab1320bab6f594dd2333bbd
TakanoriTozaki <chappy622@gmail.com>
2018-06-03 08:44:23 +0900

    file add

--- sketchList_12.js
+++ sketchList_12.js
@@ -1,22 +1,36 @@
-var angles = [ 30, 10, 45, 35, 60, 38, 75, 67];
-
 function setup() {
-	createCanvas(720,400);
-	noStroke();
-	noLoop();
+	createCanvas(720, 400);
 }
 
 function draw() {
-	background(100);
-	piChart(300, angles);
+	background(102);
+
+	push();
+	translate(width*0.2, height*0.5);
+	rotate(frameCount / 200.0);
+	polygon(0, 0, 82, 3);
+	pop();
+
+	push();
+	translate(width*0.5, height*0.5);
+	rotate(frameCount / 50.0);
+	polygon(0, 0, 80, 20);
+	pop();
+
+	push();
+	translate(width*0.8, height*0.5);
+	rotate(frameCount / -100.0);
+	polygon(0, 0, 70, 7);
+	pop();
 }
 
-function piChart(diameter, data) {
-	var lastAngle = 0;
-	for (var i = 0; i < data.length; i++) {
-		var gray = map(i, 0, data.length, 0, 255);
-		fill(gray);
-		arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
-		lastAngle += radians(angles[i]);
+function polygon(x, y, radius, npoints) {
+	var angle = TWO_PI / npoints;
+	beginShape();
+	for (var a = 0; a < TWO_PI; a += angle) {
+		var sx = x + cos(a) * radius;
+		var sy = y + sin(a) * radius;
+		vertex(sx, sy);
 	}
+	endShape(CLOSE);
 }
