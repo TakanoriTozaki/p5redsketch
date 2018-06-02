0831bce0428282b2a5dcca0da9f59913c421cff7
TakanoriTozaki <chappy622@gmail.com>
2018-06-02 13:07:21 +0900

    file add

--- sketchList_11.js
+++ sketchList_11.js
@@ -1,23 +1,22 @@
+var angles = [ 30, 10, 45, 35, 60, 38, 75, 67 ];
+
 function setup() {
-	createCanvas(720,400);
-	background(0);
+	createCanvas(720, 400);
 	noStroke();
+	noLoop();
+}
 
-	fill(204);
-	triangle(18,18,18,360,81,360);
+function draw() {
+	background(100);
+	pieChart(300, angles);
+}
 
-	fill(102);
-	rect(81,81,63,63);
-
-	fill(204);
-	quad(189,18,216,18,216,360,144,360);
-
-	fill(255);
-	ellipse(252,144,72,72);
-
-	fill(204);
-	triangle(288,18,351,360,288,360);
-
-	fill(255);
-	arc(479,300,280,280,PI,TWO_PI);
+function pieChart(diameter, data) {
+	var lastAngles = 0;
+	for (var i = 0; i < data.length; i++) {
+		var gray = map(i, 0, data.length, 0, 255);
+		fill(gray);
+		arc(width/2, height/2, diameter, diameter, lastAngles, lastAngles+radians(angles[i]));
+		lastAngles += radians(angles[i]);
+	}
 }
