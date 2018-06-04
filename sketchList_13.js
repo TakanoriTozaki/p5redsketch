93f73ca2e70256cf8cc76161970c4c49e8b53d68
TakanoriTozaki <chappy622@gmail.com>
2018-06-04 11:47:05 +0900

    file add

--- sketchList_13.js
+++ sketchList_13.js
@@ -1,36 +1,40 @@
 function setup() {
-	createCanvas(720,400);
+	createCanvas(720, 400);
 }
 
 function draw() {
 	background(102);
 
 	push();
-	translate(width*0.2,height*0.5);
+	translate(width*0.2, height*0.5);
 	rotate(frameCount / 200.0);
-	polygon(0, 0, 82, 3);
+	star(0, 0, 5, 70, 3);
 	pop();
 
 	push();
-	translate(width*0.5,height*0.5);
+	translate(width*0.5, height*0.5);
 	rotate(frameCount / 50.0);
-	polygon(0, 0, 80, 20);
+	star(0, 0, 80, 100, 40);
 	pop();
 
 	push();
-	translate(width*0.8,height*0.5);
+	translate(width*0.8, height*0.5);
 	rotate(frameCount / -100.0);
-	polygon(0, 0, 70, 70);
+	star(0, 0, 30, 70, 5);
 	pop();
 }
 
-function polygon(x, y, radius, npoints) {
+function star(x, y, radius1, radius2, npoints) {
 	var angle = TWO_PI / npoints;
+	var halfAngle = angle/2.0;
 	beginShape();
 	for (var a = 0; a < TWO_PI; a += angle) {
-		var sx = x + cos(a) * radius;
-		var sy = y + sin(a) * radius;
-		vertex(sx,sy);
+		var sx = x + cos(a) * radius2;
+		var sy = y + sin(a) * radius2;
+		vertex(sx, sy);
+		sx = x + cos(a+halfAngle) * radius1;
+		sy = y + sin(a+halfAngle) * radius1;
+		vertex(sx, sy);
 	}
 	endShape(CLOSE);
 }
