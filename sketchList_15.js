21cf32074b50c45f9fcc32ed5e16ff3900d7e7d6
TakanoriTozaki <chappy622@gmail.com>
2018-06-06 13:54:12 +0900

    file add

--- sketchList_15.js
+++ sketchList_15.js
@@ -1,31 +1,12 @@
-var x;
-var y;
-var outsideRadius = 150;
-var insideRadius = 100;
-
 function setup() {
 	createCanvas(720, 400);
-	background(204);
-	x = width/2;
-	y = height/2;
+	stroke(255);
+	noFill();
 }
 
 function draw() {
-	background(204);
-
-	var numPoints = int(map(mouseX, 0, width, 6, 60));
-	var angle = 0;
-	var angleStep = 180.0/numPoints;
-	beginShape(TRIANGLE_STRIP);
-	for (var i = 0; i <= numPoints; i++) {
-		var px = x + cos(radians(angle)) * outsideRadius;
-		var py = y + sin(radians(angle)) * outsideRadius;
-		angle += angleStep;
-		vertex(px,py);
-		px = x + cos(radians(angle)) * insideRadius;
-		py = y + sin(radians(angle)) * insideRadius;
-		vertex(px,py);
-		angle += angleStep;
+	background(0);
+	for (var i = 0; i < 200; i += 20) {
+		bezier(mouseX-(i/2.0), 40+i, 410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0));
 	}
-	endShape();
 }
