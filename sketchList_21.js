fa1a1c86eeec0d373f62d5bf299a5e4a576530f9
TakanoriTozaki <chappy622@gmail.com>
2018-06-12 11:41:46 +0900

    file add

--- sketchList_21.js
+++ sketchList_21.js
@@ -1,7 +1,7 @@
 var coswave = [];
 
 function setup() {
-	createCanvas(720,360);
+	createCanvas(720, 360);
 	for (var i = 0; i < width; i++) {
 		var amount = map(i, 0, width, 0, PI);
 		coswave[i] = abs(cos(amount));
@@ -15,20 +15,20 @@ function draw() {
 	var y2 = height/3;
 	for (var i = 0; i < width; i+=3) {
 		stroke(coswave[i]*255);
-		line(i,y1,i,y2);
+		line(i, y1, i, y2);
 	}
 
 	y1 = y2;
 	y2 = y1 + y1;
 	for (var i = 0; i < width; i+=3) {
-		stroke(coswave[i]*255/4);
-		line(i,y1,i,y2);
+		stroke(coswave[i]*255 / 4);
+		line(i, y1, i, y2);
 	}
 
 	y1 = y2;
 	y2 = height;
 	for (var i = 0; i < width; i+=3) {
-		stroke(coswave[i]*255);
-		line(i,y1,i,y2);
+		stroke(255 - coswave[i]*255);
+		line(i, y1, i, y2);
 	}
 }
