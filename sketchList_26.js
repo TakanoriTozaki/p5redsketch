352266f52bb5741e5c6a654fd80cf17b3ff89b1e
TakanoriTozaki <chappy622@gmail.com>
2018-06-17 12:47:15 +0900

    file add

--- sketchList_26.js
+++ sketchList_26.js
@@ -1,16 +1,16 @@
 function setup() {
-	createCanvas(720,360);
+	createCanvas(720, 360);
 	background(0);
 
-	for (var i = 10; i < width; i += 10) {
+	for (var i = 20; i < width; i += 10) {
 		// If 'i' divides by 20 with no remainder draw the first line
 		// else draw the second line
 		if (i%20 == 0) {
 			stroke(255);
 			line(i, 80, i, height/2);
-		}else{
+		} else {
 			stroke(153);
-			line(i,20,i,180);
+			line(i, 20, i, 180);
 		}
 	}
 }
