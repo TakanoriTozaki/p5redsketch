5d46f33b55fb701e5ddc9f5bc83136cd1b91efe0
TakanoriTozaki <chappy622@gmail.com>
2018-06-18 10:48:40 +0900

    file add

--- sketchList_27.js
+++ sketchList_27.js
@@ -1,19 +1,19 @@
 function setup() {
-	createCanvas(720,360);
+	createCanvas(720, 360);
 	background(0);
 
-		for (var i = 2; i < width-2; i += 4) {
-			// If 'i' divides by 20 with no remainder
-			if ((i%20) == 0) {
-				stroke(255);
-				line(i, 80, i, height/2);
-			// If 'i' divides by 10 width no remainder
-			}else if ((i%10)==0){
-				stroke(153);
-				line(i,20,i,180);
-			// If neither of above two conditions are met
+	for (var i = 2; i < width-2; i += 4) {
+		// if 'i' divides by 20 with no remainder
+		if ((i % 20) == 0) {
+			stroke(255);
+			line(i, 80, i, height/2);
+			// if 'i' divides by 10 with no remainder
+		} else if ((i % 10) == 0) {
+			stroke(153);
+			line(i, 20, i, 180);
+			// if neither of the above two coditions are met
 			// then draw this line
-			}else{
+		} else {
 			stroke(102);
 			line(i, height/2, i, height-20);
 		}
