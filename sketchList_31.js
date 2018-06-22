651c646080cd8847ba7d9b9f110a1c6bfb2702fa
TakanoriTozaki <chappy622@gmail.com>
2018-06-22 13:46:58 +0900

    file add

--- sketchList_31.js
+++ sketchList_31.js
@@ -1,15 +1,14 @@
 var img;
 var offset = 0;
-var easing = 0.05;
+var easing = 1.0;
 
 function setup() {
-	createCanvas(720, 400);
-	img = loadImage("mem.png"); // Load an image
+	createCanvas(720, 360);
+	img = loadImage("assets/luka.jpg"); // load an image int the
 }
-
 function draw() {
 	image(img, 0, 0); // Display at full opacity
-	var dx = (mouseX-img.widht/2) - offset;
+	var dx = (mouseX-img.width/2) - offset;
 	offset += dx * easing;
 	tint(255, 127); // Display at half opacity
 	image(img, offset, 0);
