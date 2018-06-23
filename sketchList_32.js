2881425f1ca3f9f9832e936b07c129f04e642b1b
TakanoriTozaki <chappy622@gmail.com>
2018-06-23 12:09:30 +0900

    file add

--- sketchList_32.js
+++ sketchList_32.js
@@ -1,20 +1,19 @@
-var img; // Declare variable 'img'.
+var img;
+var imgMask;
+
+function preload() {
+	img = loadImage("assets/luka.jpg");
+	imgMask = loadImage("assets/mask.png");
+}
 
 function setup() {
-	createCanvas(720, 400);
-	img = createImage(230, 230);
-	img.loadPixels();
-	for(var x = 0; x < img.width; x++) {
-		for(var y = 0; y < img.height; y++) {
-			var a = map(y, 0, img.height, 255, 0);
-			img.set(x, y, [0, 153, 204, a]);
-		}
-	}
-	img.updatePixels();
+	createCanvas(720, 360);
+	img.mask(imgMask);
+	imageMode(CENTER);
 }
 
 function draw() {
-	background(0);
-	image(img, 90, 80);
-	image(img, mouseX-img.width/2, mouseY-img.height/2);
+	background(0, 102, 153);
+	image(img, width/2, height/2);
+	image(img, mouseX, mouseY);
 }
