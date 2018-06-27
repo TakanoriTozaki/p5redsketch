176ae7f8bdc397b249479c05c56fc3fc29cc0b50
TakanoriTozaki <chappy622@gmail.com>
2018-06-27 15:02:30 +0900

    file add

--- sketchList_36.js
+++ sketchList_36.js
@@ -1,39 +1,18 @@
-var img;
-
-function preload() {
-	img = loadImage("assets/lunar.jpg");
-}
+var barWidth = 20;
+var lastBar = -1;
 
 function setup() {
-	createCanvas(480, 240);
-	pixelDensity(1);
-	img.loadPixels();
-	loadPixels();
+	createCanvas(720, 360);
+	colorMode(HSB, width, height, 100);
+	noStroke();
 }
 
 function draw() {
-	for (var x = 0; x < img.width; x++) {
-		for (var y = 0; y < img.height; y++) {
-			// Calculate the 1D location from a 2D grid
-			var loc = (x + y*img.width)*4;
-			// Get the R,G,B values from image
-			var r,g,b;
-			r = img.pixels[loc];
-			// Calculate an amount to change brightness based proximity
-			var maxdist = 50;
-			var d = dist(x, y, mouseX, mouseY);
-			var adjustbrightness = 255*(maxdist-d)/maxdist;
-			r += adjustbrightness;
-			// Constrain RGB to make sure they are within 0-255 color range
-			r = Constrain(r, 0, 255);
-			// Make a new color and set pixel in the window
-			// color c = color(r, g, b);
-			var pixloc = (y*width + x)*4;
-			pixels[pixloc] = r;
-			pixels[pixloc+1] = r;
-			pixels[pixloc+2] = r;
-			pixels[pixloc+3] = 255;
-		}
+	var whichBar = mouseX / barWidth;
+	if (whichBar != lastBar) {
+		var barX = whichBar * barWidth;
+		fill(barX, mouseY, 66);
+		rect(barX, 0, barWidth, height);
+		lastBar = whichBar;
 	}
-	updatePixels();
 }
