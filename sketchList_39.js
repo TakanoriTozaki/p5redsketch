a46206c7f5418ffb7628cba13dd85ac66c1ad801
TakanoriTozaki <chappy622@gmail.com>
2018-07-01 19:02:40 +0900

    file add

--- sketchList_39.js
+++ sketchList_39.js
@@ -1,47 +1,28 @@
-// Constants
-var Y_AXIS = 1;
-var X_AXIS = 2;
-var b1, b2, c1, c2;
+var a, b, c, d, e;
 
 function setup() {
-	createCanvas(640, 400);
-
-	// Define colors
-	b1 = color(255);
-	b2 = color(0);
-	c1 = color(204, 102, 0);
-	c2 = color(0, 102, 153);
-
-	// noLoop();
+	createCanvas(720, 360);
+	noStroke();
+	a = color(165, 167, 20);
+	b = color(77, 86, 59);
+	c = color(42, 106, 105);
+	d = color(165, 89, 20);
+	e = color(146, 150, 127);
+	noLoop(); // Draw only one time
 }
 
 function draw() {
-	// Background
-	setGradient(0, 0, width/2, height, b1, b2, X_AXIS);
-	setGradient(width/2, 0, width/2, height, b2, b1, X_AXIS);
-	// Foreground
-	setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
-	setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
+	drawBand(a, b, c, d, e, 0, width/128);
+	drawBand(c, a, d, b, e, height/2, width/128);
 }
 
-function setGradient(x, y, w, h, c1, c2, axis) {
-
-	noFill();
-
-	if (axis == Y_AXIS) { // Top to bottom gradient
-		for (var i = y; i <= y+h; i++) {
-			var inter = map(i, y, y+h, 0, 1);
-			var c = lerpColor(c1, c2, inter);
-			stroke(c);
-			line(x, i, x+w, i);
-		}
-	}
-	else if (axis == X_AXIS) { // Left to right gradient
-		for (var i = x; i <= x+w; i++) {
-			var inter = map(i, x, x+w, 0, 1);
-			var c = lerpColor(c1, c2, inter);
-			stroke(c);
-			line(i, y, i, y+h);
+function drawBand(v, w, x, y, z, ypos, barWidth) {
+	var num = 5;
+	var colorOrder = [ v, w, x, y, z];
+	for(var i = 0; i < width; i += barWidth*num) {
+		for(var j = 0; j < num; j++) {
+			fill(colorOrder[j]);
+			rect(i+j*barWidth, ypos, barWidth, height/2);
 		}
 	}
 }
--- sketchList_40.js
+++ sketchList_40.js
@@ -1,28 +1,47 @@
-var dim;
+// Constants
+var Y_AXIS = 1;
+var X_AXIS = 2;
+var b1, b2, c1, c2;
 
 function setup() {
-  createCanvas(710, 400);
-  dim = width/2;
-  background(0);
-  colorMode(HSB, 360, 100, 100);
-  noStroke();
-  ellipseMode(RADIUS);
-  frameRate(30);
+  createCanvas(720, 360);
+
+  // Define Colors
+  b1 = color(255);
+  b2 = color(0);
+  c1 = color(204, 102, 0);
+  c2 = color(0, 102, 153);
+
+  noLoop();
 }
 
 function draw() {
-  background(0);
-  for (var x = 0; x <= width; x+=dim) {
-    drawGradient(x, height/2);
-  }
+  // Background
+  setGradient(0, 0, width/2, height, b1, b2, X_AXIS);
+  setGradient(width/2, 0, width/2, height, b2, b1, X_AXIS);
+  // Foreground
+  setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
+  setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
 }
 
-function drawGradient(x, y) {
-  var radius = dim/2;
-  var h = random(0, 360);
-  for (var r = radius; r > 0; --r) {
-    fill(h, 90, 90);
-    ellipse(x, y, r, r);
-    h = (h + 1) % 360;
+function setGradient(x, y, w, h, c1, c2, axis) {
+
+  noFill();
+
+  if (axis == Y_AXIS) { // Top to bottom gradient
+    for (var i = y; i <= y+h; i++) {
+      var inter = map(i, y, y+h, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x+w, i);
+    }
+  }
+  else if (axis == X_AXIS) { // Left to right gradient
+    for (var i = x; i <= x+w; i++) {
+      var inter = map(i, x, x+w, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y+h);
+    }
   }
 }
