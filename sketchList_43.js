745daa9c8b8695ed09664b3b219475cefcccd9b4
TakanoriTozaki <chappy622@gmail.com>
2018-07-04 11:34:56 +0900

    file add

--- sketchList_43.js
+++ sketchList_43.js
@@ -1,29 +1,37 @@
-function setup() {
-  createCanvas(710, 400);
-  background(51);
-  noFill();
+var a;
+var b;
+var direction;
 
+function setup() {
+  createCanvas(720, 360);
+  colorMode(RGB, width);
+  a = 0;
+  b = width;
+  direction = true;
+  frameRate(30);
 }
 
 function draw() {
-  stroke(51);
-
-  stroke(204);
-  for (var i = 0; i < width-20; i += 4) {
-    if (i > 30 + 70) {
-      line(i, 0, i, 50);
-    }
+  a++;
+  if(a > width) {
+    a = 0;
+    direction = !direction;
   }
-
-  stroke(255);
-  rect(4 + 2 * 8, 52, 290, 48);
-  rect((4 + 2) * 8, 100, 290, 49);
-
-  stroke(153);
-  for (var i = 0; i < width; i += 2) {
-    if (((i > 20) && (i < 50)) || ((i > 100) && (i < width-20))){
-    // if (i > 20 && i < 50 || i > 100 && i < width-20) {
-      line(i, 151, i, height-1);
-    }
+  if(direction == true) {
+    stroke(a);
+  } else {
+    stroke(width-a);
   }
+  line(a, 0, a, height/2);
+
+  b--;
+  if(b < 0) {
+    b = width;
+  }
+  if(direction == true) {
+    stroke(width-b);
+  } else {
+    stroke(b);
+  }
+  line(b, height/2+1, b, height);
 }
